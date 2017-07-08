7b0231fac98249b9182f8908f84a5f385d540352
Frank Tan <tansongyang@gmail.com>
2017-07-08 13:15:36 -0400

    Change to camel case

--- Data Structures/Stacks/game-of-two-stacks.js
+++ Data Structures/Stacks/game-of-two-stacks.js
@@ -10,32 +10,32 @@ function solve(a, b, x) {
   // We won't actually call `push` or `pop` because the arrays are backwards.
 
   /** @type {Number} The index of the top of stack A. */
-  let top_a = 0;
+  let topA = 0;
   /** @type {Number} The index of the top of stack B. */
-  let top_b = 0;
+  let topB = 0;
   /** @type {Number} The running sum of all "popped" elements. */
   let sum = 0;
   /** @type {Number} The current best score. */
   let score = 0;
 
   // Starting point: keep popping from A while valid.
-  while (top_a < a.length && sum + a[top_a] <= x) {
-    sum += a[top_a];
-    top_a++;
-    score = top_a;
+  while (topA < a.length && sum + a[topA] <= x) {
+    sum += a[topA];
+    topA++;
+    score = topA;
   }
 
   // Try combinations involving B.
   // If adding the top of B would exceed the sum, try again after "pushing" back an element from A.
-  while (top_b < b.length && (sum + b[top_b] <= x || top_a > 0)) {
-    const next_sum = sum + b[top_b];
-    if (next_sum <= x) {
-      top_b++;
-      sum = next_sum;
-      score = Math.max(score, top_a + top_b);
+  while (topB < b.length && (sum + b[topB] <= x || topA > 0)) {
+    const nextSum = sum + b[topB];
+    if (nextSum <= x) {
+      topB++;
+      sum = nextSum;
+      score = Math.max(score, topA + topB);
     } else {
-      top_a--;
-      sum -= a[top_a];
+      topA--;
+      sum -= a[topA];
     }
   }
 
